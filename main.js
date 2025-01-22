d4f177fab7d6915db084ef07ecadc2fffe3aad62
Ismael <87236716+Bendji008@users.noreply.github.com>
2025-01-22 12:48:36 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+//globasl variables
+const time_el = document.querySelector('.watch .time');
+const start_btn = document.getElementById('start');
+const stop_btn = document.getElementById('stop');
+const reset_btn = document.getElementById('reset');
+
+let seconds = 0;
+let interval = null;
+
+//Events listeners
+start_btn.addEventListener('click', start);
+stop_btn.addEventListener('click', stop);
+reset_btn.addEventListener('click', reset);
+
+//Update the timer
+function timer() {
+    seconds++;
+    //format our time
+    let hrs = Math.floor(seconds / 3600);/*every 3600 seconds will be equal to an hour*/
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60);/*every 60 seconds will be equal to a minute*/
+    let secs = seconds % 60;
+
+    if (secs < 10) secs = '0' + secs;
+    if (mins < 10) mins = '0' + mins;
+    if (hrs < 10) hrs = '0' + hrs;
+
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+}
+
+function start() {
+    if (interval){
+        return
+    }
+    interval = setInterval(timer, 1000);
+}
+function stop() {
+    clearInterval(interval);
+    interval = null;
+}
+function reset() {
+    stop();
+    seconds = 0;
+    time_el.innerText = '00:00:00';
+}
